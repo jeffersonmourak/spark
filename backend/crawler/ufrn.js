64dd673165267f1de8e89ddd26b468a095faaf19
Jefferson Moura <jefinho.moura14@gmail.com>
2017-08-30 10:16:10 -0300

    adding documentation

--- backend/crawler/ufrn.js
+++ backend/crawler/ufrn.js
@@ -3,6 +3,15 @@ const { JSDOM } = jsdom;
 const HTTP = require('../http');
 const URLS = require('./urls.json');
 
+/**
+  Acquire function
+
+  @description
+  Funcion to access and find the dynamic URL for CSV file inside open data
+  platform of UFRN
+  
+  @param {String} url Url of pafge.
+*/
 async function acquire(url) {
   let page = new JSDOM(await HTTP.get(url)),
       fileDownload = page.window.document.querySelectorAll('.resource-url-analytics')[0].href;
--- backend/http.js
+++ backend/http.js
@@ -1,6 +1,23 @@
 const request = require('request');
 
+/**
+  HTTP class
+
+  @description
+  HTTP class is a promise based handle to make http request to external servers.
+*/
 class HTTP {
+  /**
+    Request
+
+    @description
+    Peform the request based on URL and method, then returns a promise with
+    page's body
+
+    @param {String} method HTTP request method
+    @param {String} url URL to be accessed
+    @returns {Promise}
+  */
   static _request(method, url) {
     return new Promise( (resolve, reject)=> {
       request[method](url, (err, response, body) => {
@@ -12,10 +29,29 @@ class HTTP {
     } );
   }
 
+  /**
+    Get
+
+    @description
+    Peform the get request to a URL.
+
+    @param {String} url URL to be accessed
+    @returns {Promise}
+  */
   static get(url) {
     return HTTP._request('get', url);
   }
 
+  /**
+    Post
+
+    @description
+    Peform the post request to a URL.
+    @TODO add support to body data.
+
+    @param {String} url URL to be accessed
+    @returns {Promise}
+  */
   static post(url) {
     return HTTP._request('post', url);
   }
