c74d6bd989109c795ed54ae094fa26d658421d6d
Jefferson Moura <jefinho.moura14@gmail.com>
2017-09-13 13:46:55 -0300

    adding comment

--- tests_helper/helper.js
+++ tests_helper/helper.js
@@ -1,5 +1,6 @@
 require('module-alias/register');
 
+// Global function for tests
 global.apiExpose = require('@test/api-expose');
 global.TestTarget = require('@test/target');
 global.expect = require('expect.js');
